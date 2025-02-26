194c45f6099de769230ebc9a2ab328aed1337d13
Kristian Whittick <kristian.whittick@gmail.com>
2025-02-26 12:15:24 +0000

    Updated C version and removed python version

--- close-dialog/c/closed.c
+++ close-dialog/c/closed.c
@@ -1,17 +1,77 @@
 #include <windows.h>
+#include <tlhelp32.h>
+#include <stdio.h>
 
-#pragma comment(lib, "user32.lib")
+#define MAIN_CLASS_NAME "TfrmMain"
+#define MAIN_WINDOW_TITLE "Speakonia - CFS-Technologies"
+#define DIALOG_CLASS_NAME "#32770"
 
+// Function to get a window handle by class name
+HWND GetWindowByClass(const char *className)
+{
+    return FindWindowA(className, NULL);
+}
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-               PWSTR szCmdLine, int CmdShow) {
+// Function to get a window title
+void GetWindowTitle(HWND hwnd, char *title, int size)
+{
+    GetWindowTextA(hwnd, title, size);
+}
 
-    static const char DIALOG_CLASS[] = "#32770";
+// Function to get the owner of a dialog
+HWND GetDialogOwner(HWND hwndDialog)
+{
+    return GetWindow(hwndDialog, GW_OWNER);
+}
 
-    HWND FindWindowA(   LPCSTR(L"#32770"),    [in, optional] LPCSTR lpWindowName
-    );
+// Function to send ESC key to a window
+void SendEscapeKey(HWND hwnd)
+{
+    PostMessage(hwnd, WM_KEYDOWN, VK_ESCAPE, 0);
+    PostMessage(hwnd, WM_KEYUP, VK_ESCAPE, 0);
+}
 
-    MessageBoxW(NULL, szCmdLine, L"Title", MB_OK);
+int main()
+{
+    // Get the main window
+    HWND hwndMain = GetWindowByClass(MAIN_CLASS_NAME);
+    if (!hwndMain)
+    {
+        return 1; // Exit if main window not found
+    }
+
+    // Verify the main window title
+    char mainTitle[256];
+    GetWindowTitle(hwndMain, mainTitle, sizeof(mainTitle));
+    if (strcmp(mainTitle, MAIN_WINDOW_TITLE) != 0)
+    {
+        return 1; // Exit if title does not match
+    }
+
+    while (1)
+    {
+        // Find the first dialog window
+        HWND hwndDialog = GetWindowByClass(DIALOG_CLASS_NAME);
+        if (!hwndDialog)
+        {
+            return 0; // Exit if no dialogs exist
+        }
+
+        // Get the owner of the dialog
+        HWND hwndOwner = GetDialogOwner(hwndDialog);
+        char ownerTitle[256];
+        GetWindowTitle(hwndOwner, ownerTitle, sizeof(ownerTitle));
+
+        // Check if the owner is the main application window
+        if (strcmp(ownerTitle, MAIN_WINDOW_TITLE) == 0)
+        {
+            SetForegroundWindow(hwndDialog);
+            Sleep(20);
+            SendEscapeKey(hwndDialog); // Send ESC to close it
+        }
+
+        Sleep(50); // Wait 50ms before checking again
+    }
 
     return 0;
 }
--- close-dialog/c/teCl.c
+++
@@ -1,9 +0,0 @@
-#include "windows.h"
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-               PWSTR szCmdLine, int CmdShow) {
-
-    printf("Hello world");
-
-    return 0;
-}
